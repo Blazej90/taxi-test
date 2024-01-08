40c32a2e044e2b4fa07209f5db0a618bf068fad2
Błażej Bartoszewski <blazejbart@gmail.com>
2024-01-08 10:23:04 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+function validateInput(input) {
+  // Ogranicz wprowadzane wartości do liczb dodatnich
+  if (input.value < 0) {
+    input.value = 0;
+  }
+
+  // Ogranicz wprowadzane wartości do liczb całkowitych
+  input.value = Math.floor(input.value);
+}
+
+function displayResult(result) {
+  // Wyświetl wynik w polu input
+  const resultInput = document.getElementById("result");
+  resultInput.value = result;
+
+  // Sprawdź warunki i ustaw kolor pola wynikowego
+  if (Math.abs(result) > 1) {
+    resultInput.style.color = "red";
+    alert("Wynik testu negatywny");
+  } else {
+    resultInput.style.color = "green";
+    alert("Wynik testu pozytywny");
+  }
+}
+
+function calculate() {
+  // Pobierz wartości z pól input
+  const number1 = parseFloat(document.getElementById("number_1").value);
+  const number2 = parseFloat(document.getElementById("number_2").value);
+
+  // Sprawdź, czy wartości są liczbami
+  if (isNaN(number1) || isNaN(number2)) {
+    alert("Wprowadź poprawne liczby!");
+    return;
+  }
+
+  // Oblicz wynik
+  const result = (((number1 - number2) / number2) * 100).toFixed(3);
+
+  // Wywołaj funkcję do wyświetlania wyniku i alertu
+  displayResult(result);
+}
+
+// Dodana funkcja do czyszczenia inputów
+function clearInputs() {
+  const resultInput = document.getElementById("result");
+  document.getElementById("number_1").value = "";
+  document.getElementById("number_2").value = "";
+  resultInput.value = "";
+  resultInput.style.color = ""; // Przywróć domyślny kolor
+}
